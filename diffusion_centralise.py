141ccfbb35480d43d8c35071ba8544b987bba4cc
Swan <swan.blanc.pro@gmail.com>
2019-09-13 15:33:29 +0200

    Add project struct

---
+++ diffusion_centralise.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+import os
+import timeit
+import logging.handlers
+from mpi4py import MPI
+
+PYTHON_LOGGER = logging.getLogger(__name__)
+if not os.path.exists("log"):
+    os.mkdir("log")
+HDLR = logging.handlers.TimedRotatingFileHandler("log/diffusion_centralise.log",
+                                                 when="midnight", backupCount=60)
+STREAM_HDLR = logging.StreamHandler()
+FORMATTER = logging.Formatter("%(asctime)s %(filename)s [%(levelname)s] %(message)s")
+HDLR.setFormatter(FORMATTER)
+STREAM_HDLR.setFormatter(FORMATTER)
+PYTHON_LOGGER.addHandler(HDLR)
+PYTHON_LOGGER.addHandler(STREAM_HDLR)
+PYTHON_LOGGER.setLevel(logging.DEBUG)
+
+# Absolute path to the folder location of this python file
+FOLDER_ABSOLUTE_PATH = os.path.normpath(os.path.dirname(os.path.abspath(__file__)))
+
+comm = MPI.COMM_WORLD
+me = comm.Get_rank()
+size = comm.Get_size()
+print("Hi from <"+str(me)+">")
+if me == 0:
+    buf = ["coucou"]
+    print("I'm <"+str(me)+">: send " + buf[0])
+    for i in range(1, size):
+	comm.send(buf, dest=i, tag=99)
+else:
+    buf = comm.recv(source=0, tag=99)
+    print("I'm <"+str(me)+">: receive " + buf[0])
